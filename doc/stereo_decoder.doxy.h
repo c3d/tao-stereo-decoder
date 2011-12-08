d0532fe28c42d1f36bcfe9a2a3a9f63b90d62069
Jerome Forissier <jerome@taodyne.com>
2011-12-08 15:11:12 +0100

    Add license check for StereoDecoder module. Move builtins from Tao to module. Add doc. Closes #1714.

--- doc/stereo_decoder.doxy.h
+++ doc/stereo_decoder.doxy.h
@@ -70,6 +70,55 @@ right_viewpoint
  */
 decode_stereo(mode:text, w:real, h:real);
 
+/**
+ * Draw only for the specified stereoscopic point of views.
+ *
+ * @p viewpoints is a bitmask that specifies for which eyes the
+ * drawing should take place.
+ *
+ * Viewpoints are numbered from left to right. When there are no more
+ * that two viewpoints, 1 is the left eye and 2 the right eye so that:
+ *
+ * - left_viewpoint is synonym for stereo_viewpoints 1.
+ * - right_viewpoint is synonym for stereo_viewpoints 2.
+ *
+ * The following example shows a square to the left eye and a circle
+ * to the right eye (such code may be used to check if a stereoscopic
+ * setup works properly):
+@code
+clear_color 0, 0, 0, 0
+color "white"
+left_viewpoint
+    rectangle 0, 0, 100, 100
+right_viewpoint
+    circle 0, 0, 50
+text_box 0, 0, window_width, window_height,
+    align_center
+    vertical_align_bottom
+    font "Andika Basic", 40
+    color "white"
+    text "Left eye: square     Right eye: circle"
+@endcode
+ * @note This primitive only affects the drawing phase, not the execution
+ * phase. In other words, the code guarded by stereo_viewpoints is
+ * always executed, whatever the value @p viewpoints may be. In that
+ * respect, the stereo_viewpoint primitive behaves pretty much like
+ * @ref locally. It is only during the drawing phase that conditional
+ * drawing occurs.
+ */
+stereo_viewpoints(viewpoints:integer);
+
+/**
+ * Draw only for the left eye.
+ * Equivalent to: stereo_viewpoints 1
+ */
+left_viewpoint(B:block);
+
+/**
+ * Draw only for the right eye.
+ * Equivalent to: stereo_viewpoints 2
+ */
+right_viewpoint(B:block);
 
 /**
 * @}
