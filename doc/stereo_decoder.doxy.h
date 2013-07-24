8ca73d4309718d321f6fa114e3090f4ba17f90cd
Baptiste Soulisse <baptiste.soulisse@taodyne.com>
2013-07-24 12:09:57 +0200

    Assure that texture_width and texture_height don't ignore texture unit. Fixes #3134.

--- doc/stereo_decoder.doxy.h
+++ doc/stereo_decoder.doxy.h
@@ -1,7 +1,8 @@
 /**
  * @~english
  * @taomoduledescription{StereoDecoder, StereoDecoder}
- * <tt>import StereoDecoder</tt> - Decode stereoscopic and multiview formats.@n
+ *
+ * <tt>import StereoDecoder</tt> - Decode stereoscopic and multiview formats.\n
  *
  * This module allows you to decode some stereoscopic or multiscopic
  * formats of your different medias and convert it to the current Tao
@@ -23,7 +24,8 @@
  *
  * @~french
  * @taomoduledescription{StereoDecoder, Décodeur stéréoscopique}
- * <tt>import StereoDecoder</tt> - Décode des formats stéréoscopiques et multi-vues.@n
+ *
+ * <tt>import StereoDecoder</tt> - Décode des formats stéréoscopiques et multi-vues.\n
  *
  * Ce module vous permet de décoder certains formats d'images ou de vidéos
  * stéréoscopiques, de manière à les afficher correctement dans
